c6bda852d16431711a426907b7a21bba3e3db488
Paulo Henrique <phsilva098@gmail.com>
2023-03-26 01:22:40 -0300

    criado arquivo canvas view

---
+++ POCDrawing/CanvasView.swift
@@ -0,0 +1,24 @@
+//
+//  CanvasView.swift
+//  POCDrawing
+//
+//  Created by Paulo Henrique Gomes da Silva on 26/03/23.
+//
+
+import UIKit
+
+struct TouchPoint {
+    var color: UIColor?
+    var points: [CGPoint]?
+
+    init(color: UIColor, points: [CGPoint]?) {
+        self.color = color
+        self.points = points
+    }
+}
+
+class CanvasView: UIView {
+
+
+
+}
